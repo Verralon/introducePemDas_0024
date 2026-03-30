fcbf34088cd811f9987bc4c97f58033a69821ccf
Verralon <indradiana056@gmail.com>
2026-03-30 16:48:10 +0700

    Menambahkan tampilan hasil seleksi

--- Looping_0024/switchCase.cpp
+++ Looping_0024/switchCase.cpp
@@ -36,5 +36,7 @@ void pilihanJurusan(){
 }
 
 void output(){
-
+    cout<<"\n=== HASIL SELEKSI ==="<<endl;
+    cout<<"Jurusan Pilihan : "<<namaJurusan<<endl;
+    cout<<"Status Kuota : "<<statusKuota<<endl;
 }
