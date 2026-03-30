bde2ca8e4c193d6f70aab60dd1340a29c574dc4f
Verralon <indradiana056@gmail.com>
2026-03-30 16:08:22 +0700

    Menambahkan program logika NOT

--- Looping_0024/looping.cpp
+++ Looping_0024/looping.cpp
@@ -22,3 +22,26 @@ cout<<"PERULANGAN DO WHILE"<<endl;
         x = rand()%10;
     }while(x<=5);
 }
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if(!(rerata < 60))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<endl;
+}
