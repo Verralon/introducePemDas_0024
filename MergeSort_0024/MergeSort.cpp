69cfbff94df2993cb2bd253108ecb0ae5f729b3b
Verralon <indradiana056@gmail.com>
2026-04-02 15:07:36 +0700

    Menambahkan panjang array maksimal 20

--- MergeSort_0024/MergeSort.cpp
+++ MergeSort_0024/MergeSort.cpp
@@ -6,6 +6,18 @@ int n;
 
 void input()
 {
-    cout << "Masukkan Panjang element array : ";
-    cin >> n;
+    while (true)
+    {
+         cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
 }
