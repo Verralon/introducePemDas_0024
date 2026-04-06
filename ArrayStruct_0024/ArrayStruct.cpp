b44919dca0b3d61ed0bc5591bf7c3fa6681d6d36
Verralon <indradiana056@gmail.com>
2026-04-06 10:42:16 +0700

    Mengisi array

--- ArrayStruct_0024/ArrayStruct.cpp
+++ ArrayStruct_0024/ArrayStruct.cpp
@@ -9,4 +9,7 @@ int main(){
 
     //cara akses (membaca atau mengambil isi array)
     cout << "isi data kedua : " << dataku[1] << endl;
+    //mengisi array
+    dataku[1] = 200;
+    cout << "isi data kedua setelah diubah : " << dataku[1] << endl;
 }
