a0fdb2798167390ecda3a805521c20f712268686
Verralon <indradiana056@gmail.com>
2026-04-06 11:04:01 +0700

    mengisi data

--- ArrayStruct_0024/ArrayStruct2.cpp
+++ ArrayStruct_0024/ArrayStruct2.cpp
@@ -12,4 +12,16 @@ int main()
 {
     //deklarasi variabel struct
     mahasiswa mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Usia : " << mhs.umur << endl;
 }
