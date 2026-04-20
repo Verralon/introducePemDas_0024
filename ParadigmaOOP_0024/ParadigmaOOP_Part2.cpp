c780d5b4537e8ddab4e66ff879b98c32c3e582ff
Verralon <indradiana056@gmail.com>
2026-04-20 11:36:21 +0700

    menambahkan teks output

--- ParadigmaOOP_0024/ParadigmaOOP_Part2.cpp
+++ ParadigmaOOP_0024/ParadigmaOOP_Part2.cpp
@@ -8,4 +8,11 @@ class Barang{
         float jumlah;
         string kategori;
         float tanggalProduksi;
-}
+    void printData()
+    {
+        cout << "Nama: " << nama << endl;
+        cout << "Jumlah: " << jumlah << endl;
+        cout << "Kategori: " << kategori << endl;
+        cout << "TanggalProduksi" << tanggalProduksi << endl;
+    }
+};
