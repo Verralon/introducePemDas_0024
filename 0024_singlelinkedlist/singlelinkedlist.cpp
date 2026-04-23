ec003e9ff4fc6bafd9b254aaaff4d5a8f3948b5c
Verralon <indradiana056@gmail.com>
2026-04-23 16:39:15 +0700

    implement basic addNode for sorted insertion

--- 0024_singlelinkedlist/singlelinkedlist.cpp
+++ 0024_singlelinkedlist/singlelinkedlist.cpp
@@ -14,3 +14,24 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout <<"\nMaskukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        node *nodeBaru = new node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan" << endl;
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
