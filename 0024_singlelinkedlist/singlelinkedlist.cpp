15ac943e750fd4f59264c5fcde85bd7d8bc41181
Verralon <indradiana056@gmail.com>
2026-04-23 16:43:46 +0700

    Add delNote

--- 0024_singlelinkedlist/singlelinkedlist.cpp
+++ 0024_singlelinkedlist/singlelinkedlist.cpp
@@ -71,3 +71,21 @@ public:
         }
         return (current != NULL);
     }
+
+        bool delNode(int nim)
+    {
+        node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        
+        delete current;
+        return true;
+    }
+
+    
